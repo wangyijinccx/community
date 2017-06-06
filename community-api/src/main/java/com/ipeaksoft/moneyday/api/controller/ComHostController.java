934a40ebb53d8c16f6cb26f768caf827aac85da8
wangyijinccx <897168791@qq.com>
2017-06-06 14:50:09 +0800

    host

--- community-api/src/main/java/com/ipeaksoft/moneyday/api/controller/ComHostController.java
+++ community-api/src/main/java/com/ipeaksoft/moneyday/api/controller/ComHostController.java
@@ -31,6 +31,11 @@ public class ComHostController extends BaseController {
 	@Autowired
 	CommHostService commHostService;
 
+	/**
+	 * 获取主播列表
+	 * 
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping("getlist")
 	public Object getlist() {
@@ -80,30 +85,69 @@ public class ComHostController extends BaseController {
 		return result;
 	}
 
+	/**
+	 * 绑定主播
+	 * 
+	 * @param hostid
+	 * @param token
+	 * @param response
+	 * @return
+	 */
 	@ResponseBody
-	@RequestMapping("bindingHost")
-	public Object getlist(Integer hostid,String token,
+	@RequestMapping("bindinghost")
+	public Object bindingHost(Integer hostid, String token,
 			HttpServletResponse response) {
 		JSONObject result = new JSONObject();
 		CommUser commUser = new CommUser();
 		commUser.setIndicate(token);
 		commUser.setCommid(hostid);
-		if(commUserService.updateByIndicate(commUser)<1){
+		if (commUserService.updateByIndicate(commUser) < 1) {
 			result.put("result", 2);
 			result.put("msg", "绑定失败");
 			return result;
 		}
-		CommHost commHost = commHostService.selectByPrimaryKey(hostid);
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("id", commHost.getId());
-		map.put("webinarId", commHost.getWebinarId());
-		map.put("subject", commHost.getSubject());
-		map.put("imgUrl", commHost.getImgUrl());
-		map.put("onlinestatus", commHost.getOnlinestatus());
-		map.put("nickname", commHost.getNickname());
+		/*
+		 * CommHost commHost = commHostService.selectByPrimaryKey(hostid);
+		 * Map<String, Object> map = new HashMap<String, Object>();
+		 * map.put("id", commHost.getId()); map.put("webinarId",
+		 * commHost.getWebinarId()); map.put("subject", commHost.getSubject());
+		 * map.put("imgUrl", commHost.getImgUrl()); map.put("onlinestatus",
+		 * commHost.getOnlinestatus()); map.put("nickname",
+		 * commHost.getNickname());
+		 */
 		result.put("result", 1);
-		result.put("webinar", map);
+		// result.put("webinar", map);
 		result.put("msg", "绑定成功");
+		return result;
+	}
+
+	@ResponseBody
+	@RequestMapping("getrecords")
+	public Object getRecords(Integer webinarId, String token, Integer pos,
+			HttpServletResponse response) {
+		//不判断主播是否在线
+		JSONObject result = new JSONObject();
+		String url = "http://e.vhall.com/api/vhallapi/v2/record/list";
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("webinar_id", webinarId + "");
+		params.put("pos", pos + "");
+		params.put("limit", "30");
+		params.put("time_seq", "1");
+		params.put("auth_type", auth_type);
+		params.put("account", account);
+		params.put("password", password);
+		String callback = httpService.post(url, params);
+		JSONObject json = JSONObject.parseObject(callback);
+		if (null == json
+				|| (!"200".equals(json.getString("code")) && !"10019".equals(json
+						.getString("code")))) {
+			result.put("result", 2);
+			result.put("msg", "获取回放列表失败");
+			return result;
+		}
+		result.put("result", 1);
+		result.put("records", json);
+		result.put("msg", "获取回放列表成功");
 		return result;
 	}
 }
