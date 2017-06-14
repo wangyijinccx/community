213b4f0fa14a3e08e3daf093546a407826454030
wangyijinccx <897168791@qq.com>
2017-06-14 13:26:39 +0800

    order

--- community-core/src/main/java/com/ipeaksoft/moneyday/core/service/CommUserDayService.java
+++ community-core/src/main/java/com/ipeaksoft/moneyday/core/service/CommUserDayService.java
@@ -43,7 +43,7 @@ public class CommUserDayService extends BaseService {
 	public List<Map<String, Object>> selectIncomingList(Integer userid) {
 		return commUserDayMapper.selectIncomingList(userid);
 	}
-	
+
 	public List<Map<String, Object>> selectPromotionList(Integer userid) {
 		return commUserDayMapper.selectPromotionList(userid);
 	}
@@ -51,13 +51,14 @@ public class CommUserDayService extends BaseService {
 	public void statistical(String agentname, double real_amount) {
 		String xgAccount = strUtil.getAgentName(agentname);
 		CommUser commUser = commUserService.selectBymobile(xgAccount);
-		// 用户做任务前，肯定绑定了师傅和主播了
+		// 用户做任务前，绑定了师傅和主播了
+		// 有一个推广员没有师傅--老板账号
 		Integer promoterId = commUser.getId();
 		Integer masterId = commUser.getPid();
 		Integer hostId = commUser.getCommid();
 		BigDecimal realAmonut = new BigDecimal(real_amount);
 		BigDecimal promoterAward = realAmonut.multiply(promoterCommission);
-		BigDecimal masterAward = realAmonut.multiply(masterCommission);
+		// BigDecimal masterAward = realAmonut.multiply(masterCommission);
 		BigDecimal hostAward = realAmonut.multiply(hostCommission);
 		// todayRegisterNum 今日注册数 todayRechargeNum 今日充值人数 todayCommission 今日收益总数
 		// todayRechargeTotal 今日充值总数(推广员玩家) todayRechargeTd 今日徒弟玩家充值总数
@@ -90,33 +91,37 @@ public class CommUserDayService extends BaseService {
 				.doubleValue());
 		commUserService.updateByPrimaryKeySelective(model);
 
-		// 推广员的师傅
-		CommUserDay masterCud = selectCurrentInfo(masterId);
-		CommUserDay masterCommUserDay = new CommUserDay();
-		masterCommUserDay.setUserid(masterId);
-		if (null == masterCud) {
-			masterCommUserDay.setTodaycommission(masterAward.doubleValue());
-			masterCommUserDay.setTodayrechargetd(real_amount);
-			masterCommUserDay.setTime(new Date());
-			commUserDayMapper.insertSelective(masterCommUserDay);
-		} else {
-			BigDecimal original = new BigDecimal(masterCud.getTodaycommission());
-			masterCommUserDay.setTodaycommission(original.add(masterAward)
-					.doubleValue());
-			masterCommUserDay.setTodayrechargetd(real_amount);
-			commUserDayMapper.updateCurrentInfo(masterCommUserDay);
+		if (null != masterId) {
+			BigDecimal masterAward = realAmonut.multiply(masterCommission);
+			// 推广员的师傅
+			CommUserDay masterCud = selectCurrentInfo(masterId);
+			CommUserDay masterCommUserDay = new CommUserDay();
+			masterCommUserDay.setUserid(masterId);
+			if (null == masterCud) {
+				masterCommUserDay.setTodaycommission(masterAward.doubleValue());
+				masterCommUserDay.setTodayrechargetd(real_amount);
+				masterCommUserDay.setTime(new Date());
+				commUserDayMapper.insertSelective(masterCommUserDay);
+			} else {
+				BigDecimal original = new BigDecimal(
+						masterCud.getTodaycommission());
+				masterCommUserDay.setTodaycommission(original.add(masterAward)
+						.doubleValue());
+				masterCommUserDay.setTodayrechargetd(real_amount);
+				commUserDayMapper.updateCurrentInfo(masterCommUserDay);
+			}
+			// 累计 aworad 余额 tbaword 徒弟收益 totalaword 总收益
+			CommUser masterCu = commUserService.selectByPrimaryKey(masterId);
+			CommUser masterModel = new CommUser();
+			masterModel.setId(masterId);
+			masterModel.setTotalaward(masterAward.add(
+					new BigDecimal(masterCu.getTotalaward())).doubleValue());
+			masterModel.setAward(masterAward.add(
+					new BigDecimal(masterCu.getAward())).doubleValue());
+			masterModel.setTdaward(masterAward.add(
+					new BigDecimal(masterCu.getTdaward())).doubleValue());
+			commUserService.updateByPrimaryKeySelective(masterModel);
 		}
-		// 累计 aworad 余额 tbaword 徒弟收益 totalaword 总收益
-		CommUser masterCu = commUserService.selectByPrimaryKey(masterId);
-		CommUser masterModel = new CommUser();
-		masterModel.setId(masterId);
-		masterModel.setTotalaward(masterAward.add(
-				new BigDecimal(masterCu.getTotalaward())).doubleValue());
-		masterModel.setAward(masterAward.add(
-				new BigDecimal(masterCu.getAward())).doubleValue());
-		masterModel.setTdaward(masterAward.add(
-				new BigDecimal(masterCu.getTdaward())).doubleValue());
-		commUserService.updateByPrimaryKeySelective(masterModel);
 
 		// 主播
 		CommUserDay hostCud = selectCurrentInfo(hostId);
@@ -152,7 +157,6 @@ public class CommUserDayService extends BaseService {
 
 	public void registered(CommUser commUser) {
 		// 玩家 玩家所属推广员+1 推广员所属主播+1 一天一个user只有一条记录
-		// 用户做任务前，肯定绑定师傅和主播了
 		Integer hostId = commUser.getCommid();
 		Integer promotersId = commUser.getId();
 		registered_update(promotersId);
