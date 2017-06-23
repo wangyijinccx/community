2fe7a691e46314c1066a4d559afd6194f93a5dca
wangyijinccx <897168791@qq.com>
2017-06-23 17:31:21 +0800

    android send

--- community-core/src/main/java/com/ipeaksoft/moneyday/core/util/HttpRequest.java
+++ community-core/src/main/java/com/ipeaksoft/moneyday/core/util/HttpRequest.java
@@ -12,47 +12,57 @@ import java.util.Map.Entry;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class HttpRequest {
-    
+
 	private static Logger logger = LoggerFactory.getLogger(HttpRequest.class);
-	
-    // 发送POST请求，参数为form表单形式
-    public static String postForm(String url, Map<String, Object> params){
-        String result = null;
-        StringBuilder buffer = new StringBuilder();
+
+	// 发送POST请求，参数为form表单形式
+	public static String postForm(String url, Map<String, Object> params) {
+		String result = null;
+		StringBuilder buffer = new StringBuilder();
 		try {
-			HttpClient client = new HttpClient();   
+			HttpClient client = new HttpClient();
 			PostMethod method = new PostMethod(url);
-			for (Entry<String, Object>  entry: params.entrySet()) {
-			    method.addParameter(entry.getKey(), entry.getValue().toString());
-			    buffer.append(entry.getKey()+"="+entry.getValue().toString()+"&");
+			for (Entry<String, Object> entry : params.entrySet()) {
+				method.addParameter(entry.getKey(), entry.getValue().toString());
+				buffer.append(entry.getKey() + "="
+						+ entry.getValue().toString() + "&");
 			}
 			HttpMethodParams param = method.getParams();
 			param.setContentCharset("UTF-8");
 			client.executeMethod(method);
-			//打印返回的信息  
+			// 打印返回的信息
 			result = method.getResponseBodyAsString();
-			//释放连接  
+			// 释放连接
 			method.releaseConnection();
 		} catch (IllegalArgumentException e) {
 		} catch (HttpException e) {
 		} catch (IOException e) {
 		}
-		
-		if (buffer.toString().length()<=1000){
-	        logger.info("post url:{}, param:{}, result:{}", url, buffer.toString(), result);
-		}
-        return result;
-        
-    };
 
-	public static String sendHttpRequest(String sUrl, String method, String encoding){
+		if (buffer.toString().length() <= 1000) {
+			logger.info("post url:{}, param:{}, result:{}", url,
+					buffer.toString(), result);
+		}
+		return result;
+
+	};
+
+	public static String sendHttpRequest(String sUrl, String method,
+			String encoding) {
 		HttpURLConnection con = null;
 		ByteArrayOutputStream baos = null;
 		InputStream is = null;
@@ -60,49 +70,50 @@ public class HttpRequest {
 		try {
 			URL url = new URL(sUrl);
 
-			con = (HttpURLConnection)url.openConnection();
+			con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod(method);
 			con.setReadTimeout(1000 * 60 * 5);
-			
+
 			baos = new ByteArrayOutputStream();
 			is = con.getInputStream();
 			int ch;
 			byte[] tmp = new byte[1024];
-			while((ch = is.read(tmp)) != -1){
+			while ((ch = is.read(tmp)) != -1) {
 				baos.write(tmp, 0, ch);
-			} 
-			
-			if(encoding != null)
+			}
+
+			if (encoding != null)
 				sResult = new String(baos.toByteArray(), encoding);
 			else
 				sResult = new String(baos.toByteArray());
-		} catch(Exception e) {
+		} catch (Exception e) {
 			logger.error("ERROR: ", e);
-			sResult = "{\"code\":500,\"msg\":\""+e.getMessage()+"\"}";
+			sResult = "{\"code\":500,\"msg\":\"" + e.getMessage() + "\"}";
 		} finally {
-			if(is != null)
-	            try {
-	                is.close();
-                } catch (IOException e) {
-	                e.printStackTrace();
-                }
-            if(baos != null)
-	            try {
-	                baos.close();
-                } catch (IOException e) {
-	                e.printStackTrace();
-                }
-			if(con != null)
+			if (is != null)
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			if (baos != null)
+				try {
+					baos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			if (con != null)
 				con.disconnect();
 		}
-		
-		if (sResult.length()<=1000){
+
+		if (sResult.length() <= 1000) {
 			logger.info("get to url:{}, result:{}", sUrl, sResult);
 		}
 		return sResult;
 	}
-	
-	public static String sendHttpRequest(String sUrl, String method, String encoding, Map<String, String> header){
+
+	public static String sendHttpRequest(String sUrl, String method,
+			String encoding, Map<String, String> header) {
 		HttpURLConnection con = null;
 		ByteArrayOutputStream baos = null;
 		InputStream is = null;
@@ -110,54 +121,87 @@ public class HttpRequest {
 		try {
 			URL url = new URL(sUrl);
 
-			con = (HttpURLConnection)url.openConnection();
+			con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod(method);
 			con.setReadTimeout(1000 * 60 * 5);
-			
-			if( header!=null ){
+
+			if (header != null) {
 				Iterator<String> iter = header.keySet().iterator();
-				while( iter.hasNext()){
+				while (iter.hasNext()) {
 					String key = iter.next();
 					con.setRequestProperty(key, header.get(key));
 				}
 			}
-			
+
 			baos = new ByteArrayOutputStream();
 			is = con.getInputStream();
 			int ch;
 			byte[] tmp = new byte[1024];
-			while((ch = is.read(tmp)) != -1){
+			while ((ch = is.read(tmp)) != -1) {
 				baos.write(tmp, 0, ch);
-			} 
-			
-			if(encoding != null)
+			}
+
+			if (encoding != null)
 				sResult = new String(baos.toByteArray(), encoding);
 			else
 				sResult = new String(baos.toByteArray());
-			
-			if (sResult.length()<=1000){
+
+			if (sResult.length() <= 1000) {
 				logger.debug("request to url:{}, result:{}", sUrl, sResult);
 			}
-		} catch (SocketException se) {	
+		} catch (SocketException se) {
 			se.printStackTrace();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(is != null)
-	            try {
-	                is.close();
-                } catch (IOException e) {
-	                e.printStackTrace();
-                }
-            if(baos != null)
-	            try {
-	                baos.close();
-                } catch (IOException e) {
-	                e.printStackTrace();
-                }
-			if(con != null)
+			if (is != null)
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			if (baos != null)
+				try {
+					baos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			if (con != null)
 				con.disconnect();
 		}
 		return sResult;
+	}
+
+	public static void post(String url, String json, String basic) {
+		long time = System.currentTimeMillis();
+		final String CONTENT_TYPE_TEXT_JSON = "text/json";
+		CloseableHttpClient client = HttpClients.createDefault();
+		CloseableHttpResponse response = null;
+		String result = null;
+		try {
+			HttpPost httpPost = new HttpPost(url);
+			httpPost.setHeader("Content-Type", "application/json;charset=UTF-8");
+			httpPost.setHeader("Authorization", "Basic basic");
+			StringEntity se = new StringEntity(json);
+			se.setContentType(CONTENT_TYPE_TEXT_JSON);
+			httpPost.setEntity(se);
+			response = client.execute(httpPost);
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				HttpEntity entity = response.getEntity();
+				result = EntityUtils.toString(entity, "UTF-8");
+
+			}
+		} catch (Exception e) {
+			logger.error("ERROR:{}", url, e);
+		} finally {
+			try {
+				if (response != null)
+					response.close();
+			} catch (IOException e) {
+			}
+		}
+		logger.info("POST to:{}, content:{}, result:{}, time:{},code:{}", url,
+				json, result, (System.currentTimeMillis() - time), response
+						.getStatusLine().getStatusCode());
 	}
 }
--- community-core/src/main/java/com/ipeaksoft/moneyday/core/util/MessageSend.java
+++ community-core/src/main/java/com/ipeaksoft/moneyday/core/util/MessageSend.java
@@ -3,7 +3,11 @@ package com.ipeaksoft.moneyday.core.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringUtils;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import javapns.devices.Device;
 import javapns.devices.implementations.basic.BasicDevice;
@@ -13,20 +17,24 @@ import javapns.notification.PushNotificationPayload;
 import javapns.notification.PushedNotification;
 
 public class MessageSend {
+	// ios
 	public static final String Path = "";
-	public static final String key = "";
-	
+	public static final String Key = "";
+	// jpush-android
+	private final static String appKey = "7d431e42dfa6a6d693ac2d04";
+	private final static String masterSecret = "5e987ac6d2e04d95a9d8f0d1";
+
 	public static void iosSend(String deviceToken, String alert, Integer badge) {
 		// String deviceToken =
 		// "d4b3c5f3d497554f56f6f9791872666ae06e3b4e7abad6f4792dcd030007db91";
-		// String alert = "给你发信息了";// push的内容 
+		// String alert = "给你发信息了";// push的内容
 		// int badge = 3;// 图标小红圈的数值
 		String sound = "default";// 铃音
 
 		List<String> tokens = new ArrayList<String>();
 		tokens.add(deviceToken);
-		String certificatePath = Path;     // D:/ZshPush.p12
-		String certificatePassword = key; //"123456";// 此处注意导出的证书密码不能为空因为空密码会报错
+		String certificatePath = Path; // D:/ZshPush.p12
+		String certificatePassword = Key; // "123456";// 此处注意导出的证书密码不能为空因为空密码会报错
 		boolean sendCount = true;
 		try {
 			PushNotificationPayload payLoad = new PushNotificationPayload();
@@ -60,5 +68,33 @@ public class MessageSend {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	// 问题1：alias or registration_id
+	// 问题2：alert 通知的内容
+	public static void AndroidSend(String registration_id,String content) {
+		String data = appKey + ":" + masterSecret;
+		String basic = Base64.encodeBase64String(data.getBytes());
+		String url = "https://api.jpush.cn/v3/push";
+		// platform
+		JSONObject param = new JSONObject();
+		param.put("platform", "android");
+		// audience
+		JSONArray audience_reg_array = new JSONArray();
+		audience_reg_array.add(registration_id);
+		JSONObject audience_alias = new JSONObject();
+		audience_alias.put("registration_id", audience_reg_array);
+		param.put("audience", audience_alias);
+		// notification
+		JSONObject ntf_info = new JSONObject();
+		ntf_info.put("alert", content);
+		JSONObject ntf_android = new JSONObject();
+		ntf_android.put("android", ntf_info);
+		param.put("notification", ntf_android);
+		HttpRequest.post(url, param.toString(), basic);
+	}
+
+	public static void main(String args[]) {
+		MessageSend.AndroidSend("alist","content1231415");
 	}
 }
